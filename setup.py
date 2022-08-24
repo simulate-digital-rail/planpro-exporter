b273ca97d652a8ab8e5cf58dac9449700a9399fb
rohan.sawahn <rohan.sawahn@student.hpi.de>
2022-08-24 10:47:06 +0200

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
    name='planprogenerator',
    version='1.0',
    description='A simple toolkit to generate planpro files',
-   author='DRSS 2022',
-   packages=['planprogenerator', 'planprogenerator.model', 'planprogenerator.planproxml'],  # would be the same as name
+   author='OSM@HPI',
+   packages=['planprogenerator', 'planprogenerator.model'],
 )
