8f599eac9e69849e0d5577e55f49943a815849a3
Rohan Sawahn <49531442+rohansaw@users.noreply.github.com>
2022-09-11 08:46:19 +0200

    fixed mirrored map

--- planprogenerator/utils/helpers.py
+++ planprogenerator/utils/helpers.py
@@ -7,7 +7,7 @@ def dbref_coords(x, y, coord_representation: Literal['wgs84', 'dbref']):
     if coord_representation == 'wgs84':
         proj_wgs84 = pyproj.Proj(init="epsg:4326")
         proj_gk4 = pyproj.Proj(init="epsg:31468")
-        x_new, y_new = pyproj.transform(proj_wgs84, proj_gk4, x, y)
+        x_new, y_new = pyproj.transform(proj_wgs84, proj_gk4, y, x)
         return x_new, y_new
     else:
         return x, y
